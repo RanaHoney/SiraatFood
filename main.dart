8e12055f0456eb82ae621779e70e588404b53b45
RanaHoney <160855542+RanaHoney@users.noreply.github.com>
2025-08-30 11:19:23 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:siratfood/Common/reusable_text.dart';
+import 'package:siratfood/Constants/constants.dart';
+import 'package:siratfood/views/entrypoint.dart';
+
+Widget defaultHome = MainScreen();
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 825),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Siraat Food',
+          theme: ThemeData(
+              scaffoldBackgroundColor: kOffWhite,
+              iconTheme: const IconThemeData(color: kDark),
+              primarySwatch: Colors.grey),
+          home: defaultHome,
+        );
+      },
+    );
+  }
+}
